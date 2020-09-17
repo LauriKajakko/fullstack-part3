8b084e7f93469d2d06b396c9bf4265a0ce097c40
LauriKajakko <kajakko.lauri@gmail.com>
2020-09-17 17:56:23 +0300

    3.3

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+}) 
+
 app.get('/info', (req, res) => {
     const date = new Date()
     const today = date.toUTCString()
