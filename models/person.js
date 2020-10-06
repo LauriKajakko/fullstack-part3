46ea0c08eb2f2244636d69ceaa79e3191078883b
LauriKajakko <kajakko.lauri@gmail.com>
2020-10-06 17:45:15 +0300

    3.19

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -15,6 +16,7 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String
 })
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
